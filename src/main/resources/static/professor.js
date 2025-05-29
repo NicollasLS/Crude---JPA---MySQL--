6ff0c1565c42af7ccf188a930957024b398e3649
Nicollas LS <nicollas.lima13@gmail.com>
2025-05-28 21:52:50 -0300

    Java Web - JPA

---
+++ src/main/resources/static/professor.js
@@ -0,0 +1,34 @@
+// Primeira NavBar
+//import { Collapse, initMDB } from "mdb-ui-kit";
+//initMDB({ Collapse });
+
+
+
+// Segunda NavBar
+//import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
+//initMDB({ Dropdown, Collapse });
+
+
+
+// Chamada da API Alunor
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:8080/professores')
+    .then(response => response.json())
+    .then(data => {
+      const tbody = document.getElementById('professor-tbody');
+      data.forEach(aluno => {
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = `
+          <td>${aluno.nome}</td>
+          <td>${aluno.email}</td>
+          <td>${aluno.telefone}</td>
+        `;
+        tbody.appendChild(tr);
+      });
+    })
+    .catch(error => {
+      console.error('Erro ao buscar alunos:', error);
+      alert('Erro ao carregar a lista de alunos.');
+    });
+});
